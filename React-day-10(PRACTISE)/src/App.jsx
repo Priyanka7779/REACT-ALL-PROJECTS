7b0a2127ca21f25246e18c937664d71f052596fc
Priyanka7779 <7999priyankapatel@gmail.com>
2025-06-24 11:11:43 +0530

    PRACTISE yaa CHATBOT

---
+++ React-day-10(PRACTISE)/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Chatbox from './componets/Chatbox.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Chatbox/> 
+    </>
+  )
+}
+
+export default App
---
+++ React-day-10(PRACTISE)/src/componets/Chatbox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Chatbox.css'
+
+function Chatbox() {
+  return (
+    <>
+    <div className='container'>
+        <div className="card">
+            <div className="card-header">
+                <h2 className='bg-black min-h-9/10'>Chatbox</h2>
+            </div>
+           
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Chatbox
